833f88e4383e3c816e8bbea73e97027789295798
anupambbl <anupamimpbbl@gmail.com>
2022-09-15 13:05:35 +0530

    adding help component and it's CSS

--- React/nai_shuruwat/src/App.js
+++ React/nai_shuruwat/src/App.js
@@ -1,7 +1,8 @@
 import Navbar from './components/Navbar';
 import Footer from './components/footer/footer.js';
 import Social from './components/social media/Social.js'
-import Volunteer_form from './components/volunteer_form/volunteer_form.js'
+import Volunteer_form from './components/volunteer_form/volunteer_form.js';
+import Help from './components/Help/Help.js';
 //import logo from './logo.svg';
 //import './App.css';
 
@@ -9,10 +10,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <Navbar/>
         <Social/> 
         <Volunteer_form/>
+        <Help/>
         <Footer/>
+        
       </header>
     </div>
   );
